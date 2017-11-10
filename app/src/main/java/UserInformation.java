f1044b5bf974afb6d49804f5e72a9e92a7242f88
alex <alkonig@umich.edu>
2017-11-09 19:53:01 -0500

    Update Class

--- app/src/main/java/UserInformation.java
+++ app/src/main/java/UserInformation.java
@@ -3,4 +3,37 @@
  */
 
 public class UserInformation {
+    String FirstName;
+    String LastName;
+    Integer Age;
+    String Address;
+    String Biography;
+
+    public UserInformation(String firstName, String lastName, Integer age, String address, String biography) {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Address = address;
+        Biography = biography;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getBiography() {
+        return Biography;
+    }
 }
