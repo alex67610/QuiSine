fd1f528b0f56197978bd5ce55ed4f4255eff2615
alex <alkonig@umich.edu>
2017-11-11 12:12:03 -0500

    Add Drawables

---
+++ app/src/main/java/com/example/alex/quisine/CurrentIngredients.java
@@ -0,0 +1,13 @@
+package com.example.alex.quisine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CurrentIngredients extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_ingredients);
+    }
+}
